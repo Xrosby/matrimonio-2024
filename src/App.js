1c728f74ee741ed362dcc94989bbaefffa0111db
Mads Norby <mads.norby@eidosmedia.com>
2024-02-06 17:40:34 +0100

    new design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import castello from './castello.jpg'
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 function App() {
   const menuItems = [
     {
+      icon: <InboxIcon />,
       name: 'We\'re Getting Married!',
       id: 'getting-married',
       component:
-        <div className='content-container' id="getting-married">
+        <div id="getting-married">
           <h1>We're Getting Married!</h1>
           <div style={{ display: 'flex' }}>
             <h2 style={{ borderRight: '1px solid darkgrey', paddingRight: 10, marginRight: 10 }}>August 17th, 2024</h2><h2>Castello di San Basilio, Pisticci</h2>
@@ -14,13 +31,21 @@ function App() {
         </div>
     },
     {
+      icon: <InboxIcon />,
       name: 'Location',
       id: 'location',
       component: <div className='content-container' id="location">
         <h1>Location</h1>
+        <div style={{ display: 'flex' }}>
+          <div style={{ maxWidth: '48%', border: '2px solid black' }}>
+            <img style={{ height: '100%', width: '100%' }} src={castello} alt='Castello di San Basilio' />
+          </div>
+          <div style={{ paddingLeft: '4%' }}>The location is Castello Di San Basilio in Pisticci. Some random test! San Basilio in Pisticci. Some random test! San Basilio in Pisticci. Some random test! San Basilio in Pisticci. Some random test! San Basilio in Pisticci. Some random test! San Basilio in Pisticci. Some random test! </div>
+        </div>
       </div>
     },
     {
+      icon: <InboxIcon />,
       name: 'What To See',
       id: 'what-to-see',
       component: <div className='content-container' id="what-to-see">
@@ -28,6 +53,7 @@ function App() {
       </div>
     },
     {
+      icon: <InboxIcon />,
       name: 'RSVP',
       id: 'rsvp',
       component: <div className='content-container' id="rsvp">
@@ -35,6 +61,7 @@ function App() {
       </div>
     },
     {
+      icon: <InboxIcon />,
       name: 'Gifts',
       id: 'gifts',
       component: <div className='content-container' id="gifts">
@@ -43,7 +70,34 @@ function App() {
     },
   ]
 
-  const MenuItem = ({ name, id }) => <a style={{ textDecoration: 'none', color: 'white', fontWeight: 600, fontSize: 20 }} href={`#${id}`}>{name.toUpperCase()}</a>
+  const MenuItem = ({ name, id }) => <a style={{ textDecoration: 'none', fontWeight: 600, fontSize: 20 }} href={`#${id}`}>{name.toUpperCase()}</a>
+
+
+  const [open, setOpen] = React.useState(false)
+
+
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+    >
+      <List>
+        {menuItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton>
+              <a href={`#${item.id}`}>
+                <ListItemIcon>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.name} />
+              </a>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
 
   return (
@@ -53,16 +107,29 @@ function App() {
       <div id="main-container" style={{ height: '75%', width: '100%' }}>
         <div id="filter"></div>
         <div id="background">      </div>
-        <div id="menu">
-          {menuItems.slice(0, 2).map(menuItem => <MenuItem name={menuItem.name} id={menuItem.id} />)}
-          <div id="names">Mads - Nica</div>
-          {menuItems.slice(2, 5).map(menuItem => <MenuItem name={menuItem.name} id={menuItem.id} />)}
+
+        <div id='mobile-menu'>
+          <Button style={{ zIndex: 999, cursor: 'pointer' }} onClick={() => setOpen(true)}>
+            <div style={{ width: 30, height: 25 }}><MenuIcon style={{ color: 'white', fontSize: 50 }} /></div></Button>
+          <Drawer
+            anchor={'left'}
+            open={open}
+            onClose={() => setOpen(false)}
+          >
+            {list('left')}
+          </Drawer>
+        </div>
+        <div id="desktop-menu">
+          {menuItems.map(menuItem => <MenuItem name={menuItem.name} id={menuItem.id} />)}
         </div>
 
+        <div id="names">Mads Nica</div>
       </div>
+
       <div style={{ backgroundColor: 'white', width: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         {menuItems.map(item => item.component)}
       </div>
+
     </div >
   );
 }
